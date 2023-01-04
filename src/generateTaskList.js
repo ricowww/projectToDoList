72a2f156dfcc41946a0d6c58190c89f1ef4447e0
Federico B Dela Pena <rico.delapena.banzon@gmail.com>
2023-01-04 10:17:18 +0800

    generator of task list

---
+++ src/generateTaskList.js
@@ -0,0 +1,34 @@
+//Given an array of objects containing names, dates, tags, make vertical set of divs with an inline checkbox
+
+//for each elem, make a div
+    //will have an inline checkbox
+    //these div will have a name title div
+    //also a tag in form of a button
+function generateTaskList(tasksObjectsArray, anchorPointId){
+    //declare anchor point based on input
+    let anchorPoint = document.getElementById(anchorPointId);
+    for (let element of tasksObjectsArray){
+        //declare DOM object and its properties
+        let tempDivDomHolder = document.createElement("div");
+        tempDivDomHolder.button = document.createElement("button");
+        tempDivDomHolder.nameDiv = document.createElement("div");
+        tempDivDomHolder.dateDiv  = document.createElement("div");
+        //declare classes
+        tempDivDomHolder.classList.add("taskDiv");
+        (tempDivDomHolder.button).classList.add("taskCheck");
+        (tempDivDomHolder.nameDiv).classList.add("taskName");
+        (tempDivDomHolder.dateDiv).classList.add("taskDate");
+        //declare textcontents
+        (tempDivDomHolder.button).textContent = "O";
+        (tempDivDomHolder.nameDiv).textContent = element.name;
+        (tempDivDomHolder.dateDiv).textContent = element.dueDate;
+        //declare id's
+        tempDivDomHolder.setAttribute("id", element.id);
+        //append to html
+        tempDivDomHolder.appendChild(tempDivDomHolder.button);
+        tempDivDomHolder.appendChild(tempDivDomHolder.nameDiv);
+        tempDivDomHolder.appendChild(tempDivDomHolder.dateDiv);
+        anchorPoint.appendChild(tempDivDomHolder);
+    };
+};
+export default generateTaskList;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import moduleTest from "./moduleTest"
 import perTagList from "./perTagList"
+import generateTaskList from "./generateTaskList"
 import './styles/main.css' 
 //to get the style into the code
 
@@ -31,4 +32,29 @@ perTagList(inpAr, tagCr, outputAr);
 ///Output
 console.log(outputAr);
 //working
+//////////////////////////////////
+
+//////////////////////////////////
+//Test generateTaskList
+///Input array
+let inpArGen = [
+    {"id": "1", "name":"task1", "dueDate": "September 1, 2023", "tag": "a"},
+    {"id": "2", "name":"task2", "dueDate": "September 1, 2023", "tag": "a"},
+    {"id": "3", "name":"task3", "dueDate": "September 1, 2023", "tag": "a"},
+    {"id": "4", "name":"task4", "dueDate": "September 1, 2023", "tag": "a"},
+    {"id": "5", "name":"task5", "dueDate": "September 1, 2023", "tag": "a"},
+    {"id": "6", "name":"task6", "dueDate": "September 1, 2023", "tag": "b"},
+    {"id": "7", "name":"task7", "dueDate": "September 1, 2023", "tag": "a"},
+    {"id": "8", "name":"task8", "dueDate": "September 1, 2023", "tag": "b"},
+    {"id": "9", "name":"task9", "dueDate": "September 1, 2023", "tag": "b"},
+    {"id": "10", "name":"task10", "dueDate": "September 1, 2023", "tag": "c"},
+    {"id": "11", "name":"task11", "dueDate": "September 1, 2023", "tag": "c"}
+];
+
+//Define id to be used to call the anchorpoint
+let idCall = "test";//point to the div in the html
+///Call
+generateTaskList(inpArGen, idCall);
+///Output
+///Divs will appear in the html
 //////////////////////////////////
